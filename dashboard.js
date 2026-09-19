1ef842f145ebb480ba9cebea5dfe13847c64aa6f
Sayedh-Muhammed-Nahas-A-R <ajmirasheed51@gmail.com>
2026-09-20 01:05:50 +0530

    Upload NumeriXa project

---
+++ dashboard.js
@@ -0,0 +1,94 @@
+
+function openCalculator(type) {
+
+  // Normal Calculator 
+  if (type === "normal") {
+    window.location.href = "normalcalculator.html";
+  }
+
+  // Scientific Calculator
+  else if (type === "scientific") {
+    window.location.href = "scientificcalculator.html";
+  }
+
+  // EMI Calculator
+  else if (type === "finance") {
+    window.location.href = "finance.html";
+  }
+
+  // BMI Calculator
+  else if (type === "bmi") {
+    window.location.href = "bmi.html";
+  }
+
+  // CGPA Calculator
+  else if (type === "cgpa") {
+    window.location.href = "cgpa.html";
+  }
+
+  // Time Calculator
+  else if (type === "time") {
+    window.location.href = "time.html";
+  }
+
+  // Date Difference
+  else if (type === "date") {
+    window.location.href = "date.html";
+  }
+
+  // percentage calculator
+  else if (type === "percentage") {
+    window.location.href = "percentage.html";
+  }
+
+  // Life Timeline
+  else if (type === "lifetimeline") {
+    window.location.href = "lifetimeline.html";
+  }
+
+  // Unit Converter
+  else if (type === "unit") {
+    window.location.href = "unit.html";
+  }
+
+  // Currency Converter
+  else if (type === "currency") {
+    window.location.href = "currency.html";
+  }
+
+  // GST Calculator
+  else if (type === "gst") {
+    window.location.href = "gst.html";
+  }
+
+  // Attendance Calculator
+  else if (type === "attendance") {
+    window.location.href = "attendance.html";
+  }
+
+  // Simple Interest Calculator
+  else if (type === "simpleinterest") {
+    window.location.href = "simpleinterest.html";
+  }
+
+  // Compound Interest Calculator
+  else if (type === "compoundinterest") {
+    window.location.href = "compoundinterest.html";
+  }
+
+  // BMR Calculator
+  else if (type === "bmr") {
+    window.location.href = "bmr.html";
+  }
+
+  // attendance calculator
+  else if (type === "attendance") {
+    window.location.href = "attendance.html";
+  }
+
+  // Body Fat Calculator
+  else if (type === "bodyfat") {
+    window.location.href = "bodyfat.html";
+  }
+
+}
